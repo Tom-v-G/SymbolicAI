5905c99d75da03f55e4615999ee487e8abb4d52d
Tom <tom.v.gelooven@gmail.com>
2022-10-20 16:17:47 +0200

    Add files via upload

--- Assignment2/wumpus.pl
+++ Assignment2/wumpus.pl
@@ -10,7 +10,8 @@
 robot(1).
 goal(5).
 
-%The first link as a predicate
+%The links as a predicate
+%link(3,1).
 link(1,2).
 link(2,3).
 link(3,4).
@@ -18,4 +19,22 @@ link(3,6).
 link(6,5).
 link(6,7).
 
+%Robot functions as predicates
+adjacent(L):- robot(X), link(X, L).
 
+move(L):- adjacent(L), retractall(robot(_)), assertz(robot(L)).
+
+%suggest(L):- goal(L), adjacent(L).
+
+append([], X, X).
+append([X|Y], Z, [X|W]):- append(Y, Z, W).
+
+
+pathlink(X,Y, P):- link(X, Y), P = [Y].
+pathlink(X,Y, P):- link(X, Z), P = [Z|Q], pathlink(Z, Y, Q).
+
+path:- pathlink(X,Y, _), robot(X), goal(Y).
+
+path(P):- pathlink(X,Y, P), robot(X), goal(Y).
+
+suggest(L):- path(P), P=[L|_].
