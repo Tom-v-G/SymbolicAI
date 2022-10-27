1cb9d6904d453a3cd28eff2d03b88323e2f52186
froggypompom <100237269+froggypompom@users.noreply.github.com>
2022-10-27 20:38:46 +0200

    Update wumpus.pl

--- Assignment2/wumpus.pl
+++ Assignment2/wumpus.pl
@@ -11,13 +11,13 @@ robot(1).
 goal(5).
 
 %The links as a predicate
-%link(3,1).
-link(1,2).
-link(2,3).
-link(3,4).
-link(3,6).
-link(6,5).
-link(6,7).
+%link(3, 1).
+link(1, 2).
+link(2, 3).
+link(3, 4).
+link(3, 6).
+link(6, 5).
+link(6, 7).
 
 %Robot functions as predicates
 adjacent(L):- robot(X), link(X, L).
@@ -31,11 +31,11 @@ append([], X, X).
 append([X|Y], Z, [X|W]):- append(Y, Z, W).
 
 
-pathlink(X,Y, P):- link(X, Y), P = [Y].
-pathlink(X,Y, P):- link(X, Z), P = [Z|Q], pathlink(Z, Y, Q).
+pathlink(X, Y, P):- link(X, Y), P = [Y].
+pathlink(X, Y, P):- link(X, Z), P = [Z|Q], pathlink(Z, Y, Q).
 
-path:- pathlink(X,Y, _), robot(X), goal(Y).
+path:- pathlink(X, Y, _), robot(X), goal(Y).
 
-path(P):- pathlink(X,Y, P), robot(X), goal(Y).
+path(P):- pathlink(X, Y, P), robot(X), goal(Y).
 
 suggest(L):- path(P), P=[L|_].
