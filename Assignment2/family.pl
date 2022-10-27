3060627ee1fd51bac2cdc0d8bd4cd7a653837fc8
froggypompom <100237269+froggypompom@users.noreply.github.com>
2022-10-27 20:39:40 +0200

    Update family.pl

--- Assignment2/family.pl
+++ Assignment2/family.pl
@@ -7,23 +7,23 @@ male(peter).
 female(sofie).
 female(sandrine).
 female(fien).
-parent(joost,sacha).
-parent(joost,leon).
-parent(sandrine,sacha).
-parent(sandrine,leon).
-parent(fien,sofie).
-parent(fien,merlijn).
-parent(peter,fien).
-parent(peter,joost).
+parent(joost, sacha).
+parent(joost, leon).
+parent(sandrine, sacha).
+parent(sandrine, leon).
+parent(fien, sofie).
+parent(fien, merlijn).
+parent(peter, fien).
+parent(peter, joost).
 
 isChild(X):-parent(Y,X).
-brother(X,Y):-male(X), parent(Z, X), parent(Z, Y), \+ X = Y.
-sister(X,Y):-female(X), parent(Z, X), parent(Z, Y), \+ X = Y.
+brother(X, Y):-male(X), parent(Z, X), parent(Z, Y), \+ X = Y.
+sister(X, Y):-female(X), parent(Z, X), parent(Z, Y), \+ X = Y.
 
-cousin(X,Y):- (parent(Z, X), parent(W, Y), brother(Z, W) ); (parent(Z, X), parent(W, Y), sister(Z, W)).
+cousin(X, Y):- (parent(Z, X), parent(W, Y), brother(Z, W)); (parent(Z, X), parent(W, Y), sister(Z, W)).
 
-ancestor(X,Y):- parent(X,Y).
-ancestor(X,Y):- parent(X, Z),
+ancestor(X, Y):- parent(X, Y).
+ancestor(X, Y):- parent(X, Z),
 		ancestor(Z, Y).
 
-family(X,Y):- (ancestor(Z, X), ancestor(Z, Y)); ancestor(X, Y); ancestor(Y, X).
+family(X, Y):- (ancestor(Z, X), ancestor(Z, Y)); ancestor(X, Y); ancestor(Y, X).
